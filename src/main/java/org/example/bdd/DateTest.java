bca99cb84e01c39a14fef674606d104ab8f06e97
pattabhia <shivaram2004@gmail.com>
2021-05-22 22:53:54 +0530

    regex splitting string

--- src/main/java/org/example/bdd/DateTest.java
+++ src/main/java/org/example/bdd/DateTest.java
@@ -3,8 +3,9 @@ package org.example.bdd;
 public class DateTest {
 
     public static void main(String[] args) {
-        String EXAMPLE_TEST = "a,b,c,d,e,f";
+        String names = "a, 98,c, D, e, f";
         String regex = "^([a-zA-Z0-9]+,?\\s*)+$";
-        System.out.println(EXAMPLE_TEST.matches(regex));
+        System.out.println(names.matches(regex));
+        String [] namesArray = names.split(",");
     }
 }
